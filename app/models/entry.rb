107893a8973abc3bc7f8cc440c788202d6f97169
James Robey <james.robey@gmail.com>
2012-11-30 17:33:17 +0000

    updated ignore, added tag scope

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,8 @@ class Entry < ActiveRecord::Base
   attr_accessible :name
   after_create :set_tags
 
+  default_scope includes(:tags)
+
   def set_tags
     self.tag_list = self.name.scan(/#(\w+)/).join(', ')
     self.save
